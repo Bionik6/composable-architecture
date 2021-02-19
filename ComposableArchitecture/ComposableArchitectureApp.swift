a9ce2e05df6bf6b93fb8648c75f5738b3fa2f401
Ibrahima Ciss <bionik6@gmail.com>
2021-02-19 18:52:13 +0000

    Initial Commit

---
+++ ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by Ibrahima Ciss on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
