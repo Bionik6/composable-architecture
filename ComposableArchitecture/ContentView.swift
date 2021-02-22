dc3328d4216403456e546df3057a66a922de25ef
Ibrahima Ciss <bionik6@gmail.com>
2021-02-22 13:01:51 +0000

    Finish V1 of the view with Observable Objects

--- ComposableArchitecture/ContentView.swift
+++ ComposableArchitecture/ContentView.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
+import Combine
+
+class AppState: ObservableObject {
+  var count = 0 {
+    willSet { objectWillChange.send() }
+  }
+  
+  var objectWillChange = PassthroughSubject<Void, Never>()
+}
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @ObservedObject var state: AppState
+  
+  var body: some View {
+    NavigationView {
+      List {
+        NavigationLink(destination: CounterView(state: state)) {
+          Text("Counter Demo")
+        }
+        NavigationLink(destination: EmptyView()) {
+          Text("Favorite Prime")
+        }
+      }.navigationTitle("State Management")
     }
+  }
+}
+
+
+private func ordinal(_ n: Int) -> String {
+  let formatter = NumberFormatter()
+  formatter.numberStyle = .ordinal
+  return formatter.string(for: n) ?? ""
+}
+
+struct CounterView: View {
+  
+  @ObservedObject var state: AppState
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Button(action: { state.count -= 1 }, label: {
+          Text("-")
+        })
+        Text("\(state.count)")
+        Button(action: { state.count += 1 }, label: {
+          Text("+")
+        })
+      }
+      Button(action: { }, label: {
+        Text("Is this prime?")
+      })
+      Button(action: {}, label: {
+        Text("What is the \(ordinal(state.count)) prime?")
+      })
+    }
+    .font(.title)
+    .navigationTitle("Counter Demo")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView(state: AppState())
+  }
 }
